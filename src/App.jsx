43ffd3230cb6773f78bcfea0f7166b80e53d5551
adityabarodiya <adi25kht@gmail.com>
2024-01-05 17:05:30 +0530

    building todo app

--- src/App.jsx
+++ src/App.jsx
@@ -4,67 +4,38 @@ import viteLogo from "/vite.svg";
 import "./App.css";
 import { useEffect } from "react";
 
-var todos = [
-  {
-    title: "go to gym",
-    id: 1,
-  },
-  {
-    title: "go to gym",
-    id: 1,
-  },
-];
-
-var todo = {
-  title: "go to gym",
-  id: 1,
-};
-
-setInterval(() => {
-  todo.title = "qwerty";
-}, 1000);
-
-function App() {
-  const [count, setCount] = useState(0);
-
-  const [todo, setTodo] = useState({ title: "go to gym", id: 1 });
-  console.log("render");
+function useTodo() {
+  const [todo, setTodo] = useState([]);
 
   useEffect(() => {
     setInterval(() => {
-      setTodo({
-        title: "go to gym plese",
-        id: parseInt(Math.random()*1000),
+      fetch("http://localhost:3000/todos").then((response) => {
+        response.json().then((data) => {
+          console.log(data);
+          setTodo(data);
+        });
       });
-      console.log('-------------')
-    },1000);
+    }, 2000);
   }, []);
+
+  return todo;
+}
+
+function App() {
+  const todo = useTodo();
 
   return (
     <>
-      {todo.title}
-      <br />
-      {todo.id}
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      {todo.map((todo) => {
+        return (
+          <div>
+            {todo.title}
+            {todo.description}
+            <button>Delete</button>
+            <br />
+          </div>
+        );
+      })}
     </>
   );
 }
--- src/test.cpp
+++ src/test.cpp
@@ -5,57 +5,23 @@ using namespace std;
 #define ll long long
 #define vi vector<int>
 
-bool is_substring(const string &a, const string &b)
-{
-    /* Use std::string::find for efficient substring search */
-    return b.find(a) != string::npos;
-}
-
 void solve()
 {
     int n, m;
     cin >> n >> m;
+    set<int> st;
 
-    string x, s;
-    cin >> x >> s;
-    int c = 0;
-    bool f = false;
-    if (is_substring(s, x))
+    while (n--)
     {
-        cout << 0 << endl;
-        return;
+        int a;
+        cin >>a;
+        st.insert(a);
     }
 
-    // c++;
-    // x += x;
-    int l = x.size();
-
-
-    while (l < 4*n)
-    {
-       
-
-        if (is_substring(s, x))
-        {
-            f = 1;
-            break;
-        }
-        else
-        {
-            x += x;
-            c++;
-        }
-    }
-    if (f)
-    {
-        cout << c << endl;
-    }
+   if (st.find(m) != st.end())
+        cout << "YES\n";
     else
-    {
-        cout << -1 << endl;
-    }
-
-    l = x.size();
+        cout << "NO\n";
 }
 
 int main()
